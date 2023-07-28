dff9e4f265a2a9926ecb34bf46546ff2fbde72b6
Luca Patera <lucapatera@outlook.it>
2023-07-29 01:10:08 +0200

    Add font-awesome library

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
 import './bootstrap';
 import {createApp} from 'vue';
 import Main from "@/webapp/Main.vue";
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import {faCircleNotch} from '@fortawesome/free-solid-svg-icons';
 
-const app = createApp(Main);
-app.mount('#app');
+library.add(faCircleNotch);
+
+createApp(Main)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app');
