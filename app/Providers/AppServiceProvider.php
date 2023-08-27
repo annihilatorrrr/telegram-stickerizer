64efac80913943aee20eccb256c0397ede98bb90
Luca Patera <lucapatera@outlook.it>
2023-08-27 02:29:40 +0200

    Add additional info to about command

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Support\Stats\StatsDatabaseService;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
@@ -35,5 +36,25 @@ class AppServiceProvider extends ServiceProvider
             rewind($stream);
             return $stream;
         });
+
+        AboutCommand::add('Bot', fn () => [
+            'Version' => config('app.version'),
+            'Username' => config('bot.username'),
+            'Privacy' => config('bot.privacy'),
+            'News Channel' => config('bot.channel.username'),
+            'Support Chat' => config('bot.support'),
+            'Localization' => config('bot.localization'),
+            'Changelog' => config('bot.changelog'),
+        ]);
+
+        AboutCommand::add('Donations', fn () => [
+            'Enabled' => config('donation.enabled') ? 'Yes' : 'No',
+            'Terms' => config('donation.terms'),
+            'Telegram Provider Token' => config('donation.providers.telegram.token'),
+        ]);
+
+        AboutCommand::add('Telegram WebApp', fn () => [
+            'Main' => route('webapp.index'),
+        ]);
     }
 }
