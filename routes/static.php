f9b9e263514f638b9405e75d5f48cbea089ab1e7
Luca Patera <lucapatera@outlook.it>
2023-09-14 01:34:51 +0200

    Fix wrong route

--- routes/static.php
+++ routes/static.php
@@ -29,7 +29,7 @@ Route::group(['prefix' => 'webapp', 'as' => 'webapp.'], function () {
         Route::post('sticker/send', [WebAppController::class, 'sendSticker'])->name('sticker.send');
         Route::get('sticker/history', [WebAppController::class, 'history'])->name('sticker.history.list');
         Route::delete('sticker/history/clear', [WebAppController::class, 'clearHistory'])->name('sticker.clear.history');
-        Route::delete('sticker/history/clear', [WebAppController::class, 'clearFavorite'])->name('sticker.clear.favorite');
+        Route::delete('sticker/favorite/clear', [WebAppController::class, 'clearFavorite'])->name('sticker.clear.favorite');
         Route::get('sticker/favorite', [WebAppController::class, 'getFavoriteStickers'])->name('sticker.favorite.list');
         Route::post('sticker/favorite', [WebAppController::class, 'saveFavoriteSticker'])->name('sticker.favorite.save');
         Route::delete('sticker/favorite/{favorite}', [WebAppController::class, 'removeFavoriteSticker'])->name('sticker.favorite.delete');
