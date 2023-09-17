5f1a0004ab74bc7feb4b503c5adfcb4904192dc0
Luca Patera <lucapatera@outlook.it>
2023-09-17 22:25:12 +0200

    Update Italian translation for sticker creation instruction

--- lang/it/inline.php
+++ lang/it/inline.php
@@ -3,7 +3,7 @@
 return [
 
     'start' => 'Inizia ad usare il bot inline',
-    'create' => 'Click qui per creare il tuo sticker!',
+    'create' => 'Clicca qui per creare il tuo sticker!',
     'blocked' => 'Devi sbloccare il bot per utilizzarlo!',
     'not_started' => 'Devi avviare il bot per utilizzarlo!',
     'invalid_code' => 'Sticker non trovato!',
